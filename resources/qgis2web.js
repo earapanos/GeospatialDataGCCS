90872609c45a6f5af5b4630da675f44a3669476e
Eduardo Adriani Rapanos <52800638+earapanos@users.noreply.github.com>
2024-06-06 11:37:04 -0300

    Update qgis2web.js

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -766,9 +766,14 @@ map.on("rendercomplete", function(evt) {
         olAttribution.innerHTML = '<a href="https://openlayers.org/">OpenLayers</a> &middot; ';
         var qgisAttribution = document.createElement('li');
         qgisAttribution.innerHTML = '<a href="https://qgis.org/">QGIS</a>';
+        var linkedinAttribution = document.createElement('li');
+        linkedinAttribution.innerHTML = '<a href="https://www.linkedin.com/in/eduardo-rapanos/">EDUARDO RAPANOS</a>';
+
         attributionList.insertBefore(qgis2webAttribution, firstLayerAttribution);
         attributionList.insertBefore(olAttribution, firstLayerAttribution);
         attributionList.insertBefore(qgisAttribution, firstLayerAttribution);
+        attributionList.appendChild(linkedinAttribution);  // Adiciona o link do LinkedIn no final da lista
+
         attributionComplete = true;
     }
 })
